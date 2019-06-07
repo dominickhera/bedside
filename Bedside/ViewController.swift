655bb0730622c7ab97c5d9e8dfadc6cad438b3dd
Dominick <dominick@finnest.co>
2019-06-07 10:18:24 -0400

    Initial Commit

---
+++ Bedside/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Bedside
+//
+//  Created by Dominick Hera on 6/7/19.
+//  Copyright © 2019 Dominick Hera. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
